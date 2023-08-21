a293a71c3ee13a98e1954852a1b577b07ceba1d4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-08-20 22:01:37 -0500

    add MinHeap class

--- 230820-min-heap/main.js
+++ 230820-min-heap/main.js
@@ -2,6 +2,22 @@
 //  MAIN APPLICATION - INSERT MIN HEAP
 /////////////////////////////////////////////////
 
+class MinHeap {
+    constructor() {
+        this.heap = [undefined];
+    }
+
+    insertOne(val) {
+        let newHeap = [...this.heap, val];
+        console.log({newHeap});
+    }
+
+    print() {
+        let heap = this.heap;
+        console.log({heap});
+    }
+}
+
 const insertIntoHeap = (heap, val) => {
     let newHeap = [...heap, val];
     let childIdx = newHeap.length - 1;
@@ -32,10 +48,14 @@ console.log("****************************");
 console.log("heap: [undefined] || val: 10");
 console.log(insertIntoHeap([undefined], 10));
 
-console.log("****************************");
+console.log("\n****************************");
 console.log("heap: [undefined,10, 12, 16] || val: 8");
 console.log(insertIntoHeap([undefined, 10, 12, 16], 8));
 
-console.log("****************************");
+console.log("\n****************************");
 console.log("heap: [undefined,3, 8, 10, 11, 9, 20, 14] || val: 7");
 console.log(insertIntoHeap([undefined, 3, 8, 10, 11, 9, 20, 14], 7));
+
+console.log("\n****************************");
+const heap1 = new MinHeap();
+heap1.insertOne(10);
